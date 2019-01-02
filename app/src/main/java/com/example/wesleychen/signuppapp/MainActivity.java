ef1b3412ef2723965b2f3049ec1cf0ef83f0f88c
bait <bait@umich.edu>
2019-01-02 17:41:33 -0500

    A log message

--- app/src/main/java/com/example/wesleychen/signuppapp/MainActivity.java
+++ app/src/main/java/com/example/wesleychen/signuppapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.wesleychen.signuppapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
     }
 }
