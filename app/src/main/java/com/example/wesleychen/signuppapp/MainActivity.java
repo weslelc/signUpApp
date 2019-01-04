0656d4dd3bf2ebc005d1c0857fcf0a64cd2eef5b
Wesley <wesleylc@umich.edu>
2019-01-04 18:53:19 -0500

    connected to firebase

--- app/src/main/java/com/example/wesleychen/signuppapp/MainActivity.java
+++ app/src/main/java/com/example/wesleychen/signuppapp/MainActivity.java
@@ -4,14 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        //
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
+        //
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 }
